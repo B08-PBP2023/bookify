e9134e10e43b350e42c3dfd0065fce5b0a9d4d26
alifbintang1 <alifbintange@gmail.com>
2023-10-25 22:31:59 +0700

    membuat urls tiap aplikasi

--- profilUser/models.py
+++ profilUser/models.py
@@ -1,11 +1,11 @@
-from django.db import models
-from django.contrib.auth.models import User
+# from django.db import models
+# from django.contrib.auth.models import User
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)  
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    status = models.CharField(max_length=200, blank=True)
-    description = models.TextField(blank=True)
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)  
+#     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+#     status = models.CharField(max_length=200, blank=True)
+#     description = models.TextField(blank=True)
 
-    def __str__(self):
-        return self.user.username
+#     def __str__(self):
+#         return self.user.username
--- profilUser/urls.py
+++ profilUser/urls.py
@@ -1,16 +1,16 @@
-from django.urls import path
-from profilUser.views import view_profile
-from profilUser.views import view_profile, edit_profile
-#from . import views
+# from django.urls import path
+# from profilUser.views import view_profile
+# from profilUser.views import view_profile, edit_profile
+# #from . import views
 
-app_name = 'profilUser'
+# app_name = 'profilUser'
 
-urlpatterns = [
+# urlpatterns = [
 
-    path('view-profile/', view_profile, name='view_profile'),
-    path('edit-profile/', edit_profile, name='edit_profile'),
+#     path('view-profile/', view_profile, name='view_profile'),
+#     path('edit-profile/', edit_profile, name='edit_profile'),
     
-    # Mungkin Anda juga ingin menambahkan URL lain sesuai kebutuhan aplikasi Anda.
-]
+#     # Mungkin Anda juga ingin menambahkan URL lain sesuai kebutuhan aplikasi Anda.
+# ]
 
 
