e9a80415260121e457d322e941dd3425a63a14ad
alifbintang1 <alifbintange@gmail.com>
2023-10-27 02:04:43 +0700

    almost done

--- FAQ/models.py
+++ FAQ/models.py
@@ -1,6 +1,14 @@
 from django.db import models
-
+from pinjamBuku.models import Buku
 # Create your models here.
 
 class Question(models.Model):
     isi_pertanyaan = models.TextField()
+    judul_buku = models.TextField()
+    # buku = models.ForeignKey(Buku, on_delete=models.CASCADE)
+
+class QuestionAnswer(models.Model):
+    isi_pertanyaan = models.TextField()
+    isi_jawaban = models.TextField()
+    judul_buku = models.TextField()
+    # buku = models.ForeignKey(Buku, on_delete=models.CASCADE)
--- FAQ/views.py
+++ FAQ/views.py
@@ -1,25 +1,42 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from FAQ.models import Question
+from FAQ.models import Question, QuestionAnswer
 from django.http import HttpResponseNotFound, HttpResponseRedirect, HttpResponse
 from django.core import serializers
+from pinjamBuku.models import Buku
 
 # Create your views here.
 
-def show_page(request):
+def show_buku(request):
+    books = Buku.objects.all()
 
     context = {
-        
+        'books':books,
     }
 
+    return render(request, 'show_buku.html', context)
+
+def show_page(request, id_book):
+    context = {
+        'id_book' : id_book,
+    }
     return render(request, 'faqpage.html', context)
 
+def show_page_admin(request,id_book):
+    context = {
+        'id_book' : id_book,
+    }
+    return render(request, 'faqpage_admin.html', context)
+
 @csrf_exempt
-def add_question(request):
+def add_question(request, id_book):
     if request.method == 'POST':
+        buku = Buku.objects.get(pk=id_book)
+        
+        judul_buku = buku.title
         isi_pertanyaan = request.POST.get("isi_pertanyaan")
 
-        new_item = Question(isi_pertanyaan=isi_pertanyaan)
+        new_item = Question(isi_pertanyaan=isi_pertanyaan, judul_buku=judul_buku)
         new_item.save()
 
         return HttpResponse(b"CREATED", status=201)
@@ -34,14 +51,25 @@ def delete_question(request,id):
 
         return HttpResponse(b"DELETE", status=201)
 
+    return HttpResponseNotFound()
+
+@csrf_exempt
+def delete_question_answer(request,id):
+    if request.method == 'DELETE':
+        data = QuestionAnswer.objects.get(pk=id)
+        data.delete()
+
+        return HttpResponse(b"DELETE", status=201)
+
     return HttpResponseNotFound() 
 
-def view_list_questions(request):
+def view_list_questions(request, id_book):
 
-    questions = Question.objects.all()
+    # questions = Question.objects.all()
 
     context = {
-        'questions' : questions, 
+        # 'questions' : questions, 
+        'id_book' : id_book,
     }
 
     return render(request, 'listquestions.html', context)
@@ -50,6 +78,37 @@ def get_questions_json(request):
     questions = Question.objects.all()
     
     return HttpResponse(serializers.serialize('json', questions))
+
+def get_questions_answers_json(request):
+    questions_answers = QuestionAnswer.objects.all()
+    print(questions_answers)
+    
+    return HttpResponse(serializers.serialize('json', questions_answers))
+
+def get_questions_answers_filtered_json(request, id):
+    buku = Buku.objects.get(pk=id)
+    print(buku.title)
+    print(QuestionAnswer.objects.all())
+    questions_answers = QuestionAnswer.objects.filter(judul_buku = buku.title)
+    print(questions_answers)
+    
+    return HttpResponse(serializers.serialize('json', questions_answers))
+
+@csrf_exempt
+def jawab_question(request, id_book):
+    if request.method == 'POST':
+        buku = Buku.objects.get(pk=id_book)
+        judul_buku = buku.title
+        isi_pertanyaan = request.POST.get("isi_pertanyaan")
+        isi_jawaban = request.POST.get("isi_jawaban")
+
+
+        new_item = QuestionAnswer(isi_pertanyaan=isi_pertanyaan, isi_jawaban=isi_jawaban, judul_buku=judul_buku)
+        new_item.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
 
 
 
