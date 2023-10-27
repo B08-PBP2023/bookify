6b262fab551fa1699af176c33e67aee5f42abea0
auliarizqiqi <rizqiaulia216@gmail.com>
2023-10-27 18:31:56 +0700

    belum selesai 2

--- profilUser/views.py
+++ profilUser/views.py
@@ -1,14 +1,13 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from .models import UserProfile
 from .forms import UserProfileForm
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
 
 def show_page(request):
-
-    context = {
-        
-    }
-
+    context = {}
     return render(request, 'profil.html', context)
 
 @login_required
@@ -17,7 +16,7 @@ def profile(request):
     return render(request, 'userprofile/profil.html', {'user_profile': user_profile})
 
 @login_required
-def create_profile(request):
+def create_profil(request):
     if request.method == 'POST':
         form = UserProfileForm(request.POST, request.FILES)
         if form.is_valid():
@@ -29,14 +28,29 @@ def create_profile(request):
         form = UserProfileForm()
     return render(request, 'userprofile/create_profil.html', {'form': form})
 
-@login_required
-def edit_profile(request):
-    user_profile = UserProfile.objects.get(user=request.user)
+@csrf_exempt
+def edit_profil_ajax(request):
     if request.method == 'POST':
-        form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
-        if form.is_valid():
-            form.save()
-            return redirect('profile')
-    else:
-        form = UserProfileForm(instance=user_profile)
-    return render(request, 'userprofile/edit_profil.html', {'form': form})
+        name = request.POST.get("name")
+        status = request.POST.get("status")
+        tanggal_lahir = request.POST.get("tanggal_lahir")
+        description = request.POST.get("description")
+        foto_profil = request.POST.get("foto_profil")
+
+
+        user_profile = UserProfile.objects.get(user=request.user)
+
+        user_profile.name = name
+        user_profile.status = status
+        user_profile.tanggal_lahir = tanggal_lahir
+        user_profile.description = description
+        user_profile.foto_profil = foto_profil
+        user_profile.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
+
+def get_profilUser_json(request):
+    profil_user = UserProfile.objects.all()
+    return HttpResponse(serializers.serialize('json', profil_user))
