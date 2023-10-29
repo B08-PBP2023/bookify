18148de97a6d145a83d05d19d05e33edbd265bd5
sitaamirasyarifah <sitaamira86@gmail.com>
2023-10-29 18:35:08 +0700

    button pinjem udah bisa

--- pinjamBuku/views.py
+++ pinjamBuku/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from django.http import HttpResponse, HttpResponseNotFound
 from django.core import serializers
 from django.contrib.auth.decorators import login_required
-from pinjamBuku.models import Buku
+from pinjamBuku.models import Buku, Pinjaman
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 
@@ -21,6 +21,12 @@ def show_page(request):
 
     }
     return render(request, "catalog.html", context)
+
+def show_borrow_books(request):
+    context = {
+
+    }
+    return render(request, "borrowed_books.html", context)
 
 
 def get_borrowed_books(request):    
@@ -43,18 +49,17 @@ def delete_borrowed_books(request,id):
 @csrf_exempt
 def borrow_books(request, id_book):
     print("TESTTTTTTTTTTTTTTTTTTT")
-    if request.method == 'POST':
-        buku = Buku.objects.get(pk=id_book)
-        
-        existing_borrowed = Buku.objects.filter(user=request.user, id_book=id_book).first()
+    buku = Buku.objects.get(pk=id_book)
+    
+    existing_borrowed = Buku.objects.filter( id=id_book).first()
 
-        if existing_borrowed:
-            # Book with the same id_book already exists in Favorit
-            
-            return HttpResponse("Book is already in borrowed book list", status=201)
-        
-        print(buku.title)
-        print(type(id_book))
+    if existing_borrowed:
+        new_item = Pinjaman( user=request.user, id_book=buku.pk, title=buku.title, authors=buku.authors, language_code=buku.language_code, num_pages=buku.num_pages, publication_date=buku.publication_date, publisher=buku.publisher)
+        new_item.save()
+        return redirect('pinjamBuku:show_borrow_books')
+    
+    print(buku.title)
+    print(type(id_book))
         
         # title = models.TextField(null=True, blank=True)
         # authors = models.TextField(null=True, blank=True)
@@ -63,10 +68,6 @@ def borrow_books(request, id_book):
         # publication_date = models.TextField(null=True, blank=True)
         # publisher = models.TextField(null=True, blank=True)
     
-        new_item = Buku(user=request.user, id_book=id_book, title=buku.title, authors=buku.authors, language_code=buku.language_code, num_pages=buku.num_pages, publication_date=buku.publication_date, publisher=buku.publisher)
-        new_item.save()
 
-        return HttpResponse(b"CREATED", status=201)
 
-    return HttpResponseNotFound()
 
