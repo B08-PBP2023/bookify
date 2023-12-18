4b30fbdef6d51244e572255dda67c9d4a9988bcd
auliarizqiqi <rizqiaulia216@gmail.com>
2023-12-18 21:31:15 +0700

    profil user done

--- profilUser/urls.py
+++ profilUser/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from profilUser import views
-from profilUser.views import add_favorit, get_profilUser_json, show_buku, show_page
+from profilUser.views import add_favorit, get_profilUser_json, show_page
 from profilUser.views import show_page
 from profilUser.views import edit_profil_ajax, get_favorites, delete_favorit, get_user_profile_by_name
+from profilUser.views import add_favorit_flutter, get_favorite_by_user_flutter, delete_favorite_flutter,get_profile_flutter,edit_profile_flutter
 
 
 app_name = 'profilUser'
@@ -15,6 +16,13 @@ urlpatterns = [
     path('get_favorites/', get_favorites, name='get_favorites'),
     path('get_user_profile_by_name/',get_user_profile_by_name, name='get_user_profile_by_name'),
     path('delete_favorit/<int:id>/', delete_favorit, name = 'delete_favorit'),
+    
+    path('get_profile_flutter/', get_profile_flutter, name='get_profile_flutter'),
+    path('edit_profile_flutter/', edit_profile_flutter, name='edit_profile_flutter'),
+    path('add_favorit_flutter/<int:book_id>/', add_favorit_flutter, name = 'add_favorit_flutter'),
+    path('get_favorite_by_user_flutter/', get_favorite_by_user_flutter, name = 'get_favorite_by_user_flutter'),
+    path('delete_favorite_flutter/<int:book_id>/', delete_favorite_flutter, name = 'delete_favorite_flutter'),
+
 ]
 
 
