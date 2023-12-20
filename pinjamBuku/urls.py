cc5c328b1a9a019b12cc910163d544e12f095034
sitaamirasyarifah <sitaamira86@gmail.com>
2023-12-21 00:28:00 +0700

    sita

--- pinjamBuku/urls.py
+++ pinjamBuku/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from pinjamBuku.views import get_books, show_page, borrow_books, get_borrowed_books, delete_borrowed_books, show_borrow_books, get_books_by_judul
+from pinjamBuku.views import *
 
 app_name = 'pinjamBuku'
 
@@ -11,5 +11,9 @@ urlpatterns = [
     path('get_borrowed_books/', get_borrowed_books, name='get_borrowed_books'),
     path('delete_borrowed_books/<int:id>/', delete_borrowed_books, name = 'delete_borrowed_books'),
     path('show_borrow_books/', show_borrow_books, name='show_borrow_books'),
+    path('borrow_books_flutter/<int:book_id>/', borrow_books_flutter, name = 'borrow_books_flutter'),
+    path('get_books_by_user_flutter/', get_books_by_user_flutter, name = 'get_books_by_user_flutter'),
+    path('delete_pinjaman_flutter/<int:book_id>/', delete_pinjaman_flutter, name = 'delete_pinjaman_flutter'),
+
     
 ]
